8d03fc79a4d04bdda268a1d0028b1e3b3d7153cb
virtualonno <virtualonno@gmail.com>
2026-08-23 21:27:34 +0200

    Keep Zig rehearsal warning-clean

--- tests/templates.rs
+++ tests/templates.rs
@@ -252,6 +252,8 @@ fn cross_check_rehearses_every_non_windows_release_target() {
     assert!(cross_check.contains("--install-targets"));
     assert!(cross_check.contains("rustup target add --toolchain"));
     assert!(cross_check.contains("--release --bins --target"));
+    assert!(cross_check.contains("-Dwarnings -Alinker-messages"));
+    assert!(cross_check.contains("RUSTFLAGS=\"$cross_rustflags\" cargo zigbuild"));
 }
 
 #[test]
